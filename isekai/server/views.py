f553237af1e83c24d201d6e13df74d2d48608815
Arnav Ignatius <105193713+spartny@users.noreply.github.com>
2024-11-27 23:27:43 +0530

    deploy changes

--- isekai/server/views.py
+++ isekai/server/views.py
@@ -15,6 +15,8 @@ from django.db.models import F
 import requests
 from .models import Game, User
 from .gamebuilder import start_game, continue_game
+import os
+
 
 @api_view(['GET'])
 def test_view(request):
@@ -75,7 +77,7 @@ def oauth_success_view(request):
             'code': code,
             'client_id': settings.GOOGLE_CLIENT_ID,
             'client_secret': settings.GOOGLE_CLIENT_SECRET,
-            'redirect_uri': 'http://localhost:5173/oauth/callback/',  # Must match the original request
+            'redirect_uri': f'{os.environ.get('FRONTEND_URL')}/oauth/callback/',  # Must match the original request
             'grant_type': 'authorization_code',
         }
         token_response = requests.post(token_url, data=token_data)
